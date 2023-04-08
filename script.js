ddca9d3dace170ec4e99ec4a22b5abbf00fb11ff
saifgehlot07 <gehlotsaif07@gmail.com>
2023-04-08 23:07:27 +0530

    70% done

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-"use Strict";
-
-// Elemet Selection.
-const ctx = document.getElementById("weather__chart");
+// Element Selection.
 const windSpeed = document.getElementById("wind-speed");
+const ctx = document.getElementById("weather__chart");
 const humidity = document.getElementById("humidity");
 const pressure = document.getElementById("pressure");
 const cityName = document.getElementById("city");
-const stateName = document.getElementById("state");
+const countryName = document.getElementById("country");
 const currentTimeData = document.getElementById("current-time");
 const weatherType = document.getElementById("weather-type");
 const celciusData = document.getElementById("celcius");
@@ -14,13 +12,271 @@ const feelsLike = document.getElementById("feels-like");
 const sunriseData = document.getElementById("sunrise");
 const sunsetData = document.getElementById("sunset");
 const searchWeather = document.getElementById("search");
+const cityTime = document.getElementById("current-time");
 const API_KEY = "cdebb052c677cf0c147cc942b56bbd0d";
+
+let isoCountries = {
+  AF: "Afghanistan",
+  AX: "Aland Islands",
+  AL: "Albania",
+  DZ: "Algeria",
+  AS: "American Samoa",
+  AD: "Andorra",
+  AO: "Angola",
+  AI: "Anguilla",
+  AQ: "Antarctica",
+  AG: "Antigua And Barbuda",
+  AR: "Argentina",
+  AM: "Armenia",
+  AW: "Aruba",
+  AU: "Australia",
+  AT: "Austria",
+  AZ: "Azerbaijan",
+  BS: "Bahamas",
+  BH: "Bahrain",
+  BD: "Bangladesh",
+  BB: "Barbados",
+  BY: "Belarus",
+  BE: "Belgium",
+  BZ: "Belize",
+  BJ: "Benin",
+  BM: "Bermuda",
+  BT: "Bhutan",
+  BO: "Bolivia",
+  BA: "Bosnia And Herzegovina",
+  BW: "Botswana",
+  BV: "Bouvet Island",
+  BR: "Brazil",
+  IO: "British Indian Ocean Territory",
+  BN: "Brunei Darussalam",
+  BG: "Bulgaria",
+  BF: "Burkina Faso",
+  BI: "Burundi",
+  KH: "Cambodia",
+  CM: "Cameroon",
+  CA: "Canada",
+  CV: "Cape Verde",
+  KY: "Cayman Islands",
+  CF: "Central African Republic",
+  TD: "Chad",
+  CL: "Chile",
+  CN: "China",
+  CX: "Christmas Island",
+  CC: "Cocos (Keeling) Islands",
+  CO: "Colombia",
+  KM: "Comoros",
+  CG: "Congo",
+  CD: "Congo, Democratic Republic",
+  CK: "Cook Islands",
+  CR: "Costa Rica",
+  CI: "Cote D'Ivoire",
+  HR: "Croatia",
+  CU: "Cuba",
+  CY: "Cyprus",
+  CZ: "Czech Republic",
+  DK: "Denmark",
+  DJ: "Djibouti",
+  DM: "Dominica",
+  DO: "Dominican Republic",
+  EC: "Ecuador",
+  EG: "Egypt",
+  SV: "El Salvador",
+  GQ: "Equatorial Guinea",
+  ER: "Eritrea",
+  EE: "Estonia",
+  ET: "Ethiopia",
+  FK: "Falkland Islands (Malvinas)",
+  FO: "Faroe Islands",
+  FJ: "Fiji",
+  FI: "Finland",
+  FR: "France",
+  GF: "French Guiana",
+  PF: "French Polynesia",
+  TF: "French Southern Territories",
+  GA: "Gabon",
+  GM: "Gambia",
+  GE: "Georgia",
+  DE: "Germany",
+  GH: "Ghana",
+  GI: "Gibraltar",
+  GR: "Greece",
+  GL: "Greenland",
+  GD: "Grenada",
+  GP: "Guadeloupe",
+  GU: "Guam",
+  GT: "Guatemala",
+  GG: "Guernsey",
+  GN: "Guinea",
+  GW: "Guinea-Bissau",
+  GY: "Guyana",
+  HT: "Haiti",
+  HM: "Heard Island & Mcdonald Islands",
+  VA: "Holy See (Vatican City State)",
+  HN: "Honduras",
+  HK: "Hong Kong",
+  HU: "Hungary",
+  IS: "Iceland",
+  IN: "India",
+  ID: "Indonesia",
+  IR: "Iran, Islamic Republic Of",
+  IQ: "Iraq",
+  IE: "Ireland",
+  IM: "Isle Of Man",
+  IL: "Israel",
+  IT: "Italy",
+  JM: "Jamaica",
+  JP: "Japan",
+  JE: "Jersey",
+  JO: "Jordan",
+  KZ: "Kazakhstan",
+  KE: "Kenya",
+  KI: "Kiribati",
+  KR: "Korea",
+  KW: "Kuwait",
+  KG: "Kyrgyzstan",
+  LA: "Lao People's Democratic Republic",
+  LV: "Latvia",
+  LB: "Lebanon",
+  LS: "Lesotho",
+  LR: "Liberia",
+  LY: "Libyan Arab Jamahiriya",
+  LI: "Liechtenstein",
+  LT: "Lithuania",
+  LU: "Luxembourg",
+  MO: "Macao",
+  MK: "Macedonia",
+  MG: "Madagascar",
+  MW: "Malawi",
+  MY: "Malaysia",
+  MV: "Maldives",
+  ML: "Mali",
+  MT: "Malta",
+  MH: "Marshall Islands",
+  MQ: "Martinique",
+  MR: "Mauritania",
+  MU: "Mauritius",
+  YT: "Mayotte",
+  MX: "Mexico",
+  FM: "Micronesia, Federated States Of",
+  MD: "Moldova",
+  MC: "Monaco",
+  MN: "Mongolia",
+  ME: "Montenegro",
+  MS: "Montserrat",
+  MA: "Morocco",
+  MZ: "Mozambique",
+  MM: "Myanmar",
+  NA: "Namibia",
+  NR: "Nauru",
+  NP: "Nepal",
+  NL: "Netherlands",
+  AN: "Netherlands Antilles",
+  NC: "New Caledonia",
+  NZ: "New Zealand",
+  NI: "Nicaragua",
+  NE: "Niger",
+  NG: "Nigeria",
+  NU: "Niue",
+  NF: "Norfolk Island",
+  MP: "Northern Mariana Islands",
+  NO: "Norway",
+  OM: "Oman",
+  PK: "Pakistan",
+  PW: "Palau",
+  PS: "Palestinian Territory, Occupied",
+  PA: "Panama",
+  PG: "Papua New Guinea",
+  PY: "Paraguay",
+  PE: "Peru",
+  PH: "Philippines",
+  PN: "Pitcairn",
+  PL: "Poland",
+  PT: "Portugal",
+  PR: "Puerto Rico",
+  QA: "Qatar",
+  RE: "Reunion",
+  RO: "Romania",
+  RU: "Russian Federation",
+  RW: "Rwanda",
+  BL: "Saint Barthelemy",
+  SH: "Saint Helena",
+  KN: "Saint Kitts And Nevis",
+  LC: "Saint Lucia",
+  MF: "Saint Martin",
+  PM: "Saint Pierre And Miquelon",
+  VC: "Saint Vincent And Grenadines",
+  WS: "Samoa",
+  SM: "San Marino",
+  ST: "Sao Tome And Principe",
+  SA: "Saudi Arabia",
+  SN: "Senegal",
+  RS: "Serbia",
+  SC: "Seychelles",
+  SL: "Sierra Leone",
+  SG: "Singapore",
+  SK: "Slovakia",
+  SI: "Slovenia",
+  SB: "Solomon Islands",
+  SO: "Somalia",
+  ZA: "South Africa",
+  GS: "South Georgia And Sandwich Isl.",
+  ES: "Spain",
+  LK: "Sri Lanka",
+  SD: "Sudan",
+  SR: "Suriname",
+  SJ: "Svalbard And Jan Mayen",
+  SZ: "Swaziland",
+  SE: "Sweden",
+  CH: "Switzerland",
+  SY: "Syrian Arab Republic",
+  TW: "Taiwan",
+  TJ: "Tajikistan",
+  TZ: "Tanzania",
+  TH: "Thailand",
+  TL: "Timor-Leste",
+  TG: "Togo",
+  TK: "Tokelau",
+  TO: "Tonga",
+  TT: "Trinidad And Tobago",
+  TN: "Tunisia",
+  TR: "Turkey",
+  TM: "Turkmenistan",
+  TC: "Turks And Caicos Islands",
+  TV: "Tuvalu",
+  UG: "Uganda",
+  UA: "Ukraine",
+  AE: "United Arab Emirates",
+  GB: "United Kingdom",
+  US: "United States",
+  UM: "United States Outlying Islands",
+  UY: "Uruguay",
+  UZ: "Uzbekistan",
+  VU: "Vanuatu",
+  VE: "Venezuela",
+  VN: "Viet Nam",
+  VG: "Virgin Islands, British",
+  VI: "Virgin Islands, U.S.",
+  WF: "Wallis And Futuna",
+  EH: "Western Sahara",
+  YE: "Yemen",
+  ZM: "Zambia",
+  ZW: "Zimbabwe",
+};
 
 // Chart.js Library.
 const myChart = new Chart(ctx, {
   type: "line",
   data: {
-    labels: ["Min. temp", "Max. temp"],
+    labels: [
+      "Day 1",
+      "Day 2",
+      "Day 3",
+      "Day 4",
+      "Day 5",
+      "Day 6",
+      "Day 7",
+      "Day 8",
+    ],
     datasets: [
       {
         label: "temperature",
@@ -33,7 +289,7 @@ const myChart = new Chart(ctx, {
     scales: {
       y: {
         beginAtZero: true,
-        max: 30,
+        max: 50,
       },
     },
   },
@@ -43,14 +299,13 @@ const myChart = new Chart(ctx, {
 const showWeatherData = (data) => {
   const sunriseUnixTime = data.sys.sunrise;
   const sunsetUnixTime = data.sys.sunset;
-  const tempMin = data.main.temp_min;
-  const tempMax = data.main.temp_max;
 
   windSpeed.innerHTML = `${data.wind.speed} m/s`;
   pressure.innerHTML = `${data.main.pressure} hpa`;
   humidity.innerHTML = `${data.main.humidity}%`;
   celciusData.innerHTML = `${parseInt(data.main.temp)}°C`;
-  cityName.innerHTML = `${data.name}, ${data.sys.country}`;
+  cityName.innerHTML = `${data.name},`;
+  countryName.innerHTML = `${isoCountries[data.sys.country]}`;
   weatherType.innerHTML = `${data.weather[0].main}`;
   feelsLike.innerHTML = `${data.main.feels_like}°C`;
   sunriseData.innerHTML = `${new Date(sunriseUnixTime * 1000)
@@ -59,9 +314,27 @@ const showWeatherData = (data) => {
   sunsetData.innerHTML = `${new Date(sunsetUnixTime * 1000)
     .toLocaleTimeString()
     .substring(0, 4)} PM`;
+};
 
-  myChart.data.datasets[0].data = [tempMin, tempMax];
-  myChart.update();
+// Fetching One Call API From OpenWeather Website.
+const fetchOneCallApi = (data) => {
+  const { lat, lon } = data.coord;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((oneCallData) => {
+      cityTime.innerHTML = new Date(oneCallData.current.dt)
+        .toLocaleTimeString()
+        .substring(4, 2);
+      const drawChart = () => {
+        const chartDailyData = oneCallData.daily.map((d) => d.temp.day);
+        myChart.data.datasets[0].data = chartDailyData;
+        myChart.update();
+      };
+      drawChart();
+    });
 };
 
 // Fetching API From OpenWeather Website.
@@ -71,30 +344,24 @@ const fetchFromApi = (place) => {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       showWeatherData(data);
       fetchOneCallApi(data);
     });
 };
-
-// // Fetching One Call API From OpenWeather Website.
-// const fetchOneCallApi = (data) => {
-//   fetch(
-//     `https://api.openweathermap.org/data/3.0/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=hourly,minutly&appid=${API_KEY}`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//     });
-// };
 
 // Displaying Data When the User Inserts Value Of Any Place And Hits Enter.
 searchWeather.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
     const place = e.target.value;
     fetchFromApi(place);
+    localStorage.setItem("specificCountryData", e.target.value);
+    e.target.value = "";
   }
 });
 
-// LocalStorage.
-localStorage.removeItem(fetchFromApi(), showWeatherData());
+window.addEventListener("load", () => {
+  const storageItem = localStorage.getItem("specificCountryData");
+  if (storageItem) {
+    fetchFromApi(storageItem);
+  }
+});
